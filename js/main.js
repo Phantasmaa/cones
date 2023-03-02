3febfc239edd4c817ad6055ed4e0deba93a03d0d
Phantasma <85528963+Phantasmaa@users.noreply.github.com>
2023-03-02 00:58:02 -0500

    darkmode siuu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const contenidojudy = document.getElementById('imagejudy')
 const contenidochoco = document.getElementById('imagechoco')
+const letter=document.querySelectorAll('.letter')
 const judy= document.getElementById('judy')
 const choco= document.getElementById('choco')
 const info1= document.getElementById('judynfo')
 const info2= document.getElementById('chocoinfo')
+const mode=document.getElementById('mode')
+//const mainCont=document.getElementById('main-container')
+const encabezado=document.querySelector('.encabezado')
 
 let transformado = false;
+let darkmode=false;
 
     judy.addEventListener('click', () => {
         if (transformado) {
@@ -28,5 +33,28 @@ let transformado = false;
           info2.style.transform = 'translateY(0)'
           contenidochoco.style.filter='brightness(40%)'
           transformado = true; 
+        }
+      })
+
+      mode.addEventListener('click', () => {
+
+        if (mode.classList.contains("normalmode")) {
+          mode.classList.remove("normalmode");
+          mode.classList.add("darkmode");
+          encabezado.classList.add("darkmodecont");
+          letter[0].classList.add("letterdark");
+          letter[1].classList.add("letterdark");
+          judy.classList.add("darkmodecont")
+          choco.classList.add("darkmodecont")
+          document.body.style.background=('rgb(33,33,33)')
+        } else {
+          mode.classList.remove("darkmode");
+          mode.classList.add("normalmode");
+          encabezado.classList.remove("darkmodecont");
+          document.body.style.background=('rgb(235, 235, 235)')
+          letter[0].classList.remove("letterdark");
+          letter[1].classList.remove("letterdark");
+          judy.classList.remove("darkmodecont")
+          choco.classList.remove("darkmodecont")
         }
       })
